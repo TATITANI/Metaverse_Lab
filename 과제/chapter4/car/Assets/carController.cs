a4b5ee908ebf0b5a4b0ee729449fbbe8ce7ef4e5
tatitani <tatitani@naver.com>
2022-09-06 21:47:09 +0900

    과제4 수정

--- 과제/chapter4/car/Assets/carController.cs
+++ 과제/chapter4/car/Assets/carController.cs
@@ -1,24 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
-public class carController : MonoBehaviour {
+public class carController : MonoBehaviour
+{
     float speed = 0;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Vector2 mouseDownPos;
+    private Vector2 direction;
+    private Vector3 startPos;
+    private Vector2 movement = Vector2.zero;
+
+    private void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            this.speed = 0.2f;
-        } else if (Input.GetMouseButtonUp(0))
+            this.speed = 0;
+            mouseDownPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
         {
+            movement = mouseDownPos - (Vector2)Input.mousePosition;
+            speed = 0.002f;
             GetComponent<AudioSource>().Play();
         }
-            transform.Translate(this.speed, 0, 0);
+
+        transform.Translate(movement * speed);
         this.speed *= 0.98f;
-	}
+    }
+
+    public void Reset()
+    {
+        transform.position = startPos;
+        speed = 0;
+    }
 }
--- 과제/chapter4/car/Assets/gameDirector.cs
+++ 과제/chapter4/car/Assets/gameDirector.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,17 +9,48 @@ public class gameDirector : MonoBehaviour
     GameObject car;
     GameObject flag;
     GameObject distance;
+    GameObject arrive;
+    GameObject speed;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         this.car = GameObject.Find("car");
         this.flag = GameObject.Find("flag");
         this.distance = GameObject.Find("Distance");
+        this.arrive = GameObject.Find("Arrive");
+        this.speed = GameObject.Find("speed");
     }
-	
-	// Update is called once per frame
-	void Update () {
-        float length = this.flag.transform.position.x - this.car.transform.position.x;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = (this.flag.transform.position - this.car.transform.position).magnitude;
         this.distance.GetComponent<Text>().text = "목표지점까지 " + length.ToString("F2") + "m";
+
+        if (length < 1)
+        {
+            StartCoroutine(ShowArrivingText());
+            car.GetComponent<carController>().Reset();
+        }
+
+    }
+
+    IEnumerator ShowArrivingText()
+    {
+        this.arrive.GetComponent<Text>().text = "도착";
+        arrive.transform.position = Camera.main.WorldToScreenPoint(car.transform.position);
+        yield return new WaitForSeconds(2f);
+        this.arrive.GetComponent<Text>().text = "";
+    }
+    
+
+ 
+    public void onInput(string Value)
+    {
+        Debug.Log($"{Value}");
+        float xx;
+        float.TryParse(Value, out xx); // string -> float
+        this.car.transform.Translate(new Vector3(0, xx, 0));
     }
 }
