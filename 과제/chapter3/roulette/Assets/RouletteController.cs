b02b9e0ce8093519af2d3c96d82f0ed8b95763ba
tatitani <tatitani@naver.com>
2022-09-06 21:10:23 +0900

    과제3 수정

--- 과제/chapter3/roulette/Assets/RouletteController.cs
+++ 과제/chapter3/roulette/Assets/RouletteController.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class RouletteController : MonoBehaviour {
     float rotSpeed = 0;
     float theta = 0;
-	// Use this for initialization
-	void Start () {
-
-        transform.position = new Vector3(1f, 0, 0);
-    }
 	
-	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            this.rotSpeed = 0.1f;
+            this.rotSpeed = 10;
         }
-        float dx, dy;
-        dx = Mathf.Cos(theta * 2f * 3.141592f / 180.0f);
-        dy = Mathf.Sin(theta * 2f * 3.141592f / 180.0f);
-        transform.Translate(dx, dy, 0);
-        
+        transform.Rotate(0, 0, this.rotSpeed);
         this.rotSpeed *= 0.96f;
-
+    
     }
+    
+    /// <summary>
+    /// 원 운동 기존 코드
+    /// </summary>
+	// void Update () {
+ //        if (Input.GetMouseButtonDown(0))
+ //        {
+ //            this.rotSpeed = 0.1f;
+ //        }
+ //
+ //        this.rotSpeed *= 0.96f;
+ //        float dx, dy;
+ //
+ //        dx = Mathf.Cos(theta * 2f * 3.141592f / 180.0f);
+ //        dy = Mathf.Sin(theta * 2f * 3.141592f / 180.0f);
+ //        transform.Translate(dx, dy, 0);
+ //    }
 }
