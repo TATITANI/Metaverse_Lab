f5674b871004145e4c4fb1ff5efafb77cd6b5211
SNG0407 <kps1225@naver.com>
2022-10-27 01:04:06 +0900

    UI_Test : EMG UI Update
    UI_Test : EMG UI Update

--- Haptic_Project/Assets/UImanager.cs
+++ Haptic_Project/Assets/UImanager.cs
@@ -6,20 +6,25 @@ using TMPro;
 public class UImanager : MonoBehaviour
 {
     [SerializeField] float timeStart;
+    [SerializeField] float EMGStart; //아두이노에서 받아오기 
     //[SerializeField] TextMesh Text_time, startPauseText;
-    [SerializeField] TextMeshProUGUI Text_time, startPauseText;
+    [SerializeField] TextMeshProUGUI Text_time, Time_startPauseText;
+    [SerializeField] TextMeshProUGUI Text_EMG, EMG_startPauseText;
 
     bool timeActive = false;
+    bool EMG_Active = false;
 
     // Start is called before the first frame update
     void Start()
     {
         Text_time.text = timeStart.ToString("F2");    
+        Text_EMG.text = EMGStart.ToString("F2");    
     }
  // Update is called once per frame
     void Update()
     {
         StartTime();
+        EMG_StartEMG();
     }
 
     void StartTime()
@@ -31,18 +36,42 @@ public class UImanager : MonoBehaviour
         }
     }
 
-    public void StartPauseBtn()
+    public void Time_StartPauseBtn()
     {
         timeActive = !timeActive;
-        startPauseText.text = timeActive ? "PAUSE" : "START";
+        Time_startPauseText.text = timeActive ? "PAUSE" : "START";
     }
 
-    public void ResetBtn()
+    public void Time_ResetBtn()
     {
         if(timeStart > 0)
         {
             timeStart = 0f;
             Text_time.text = timeStart.ToString("F2");
+        }
+    }
+    void EMG_StartEMG()
+    {
+        //추후 아두이노에서 EMG를 받아오는 함수로 변경
+        if (EMG_Active)
+        {
+            EMGStart += Time.deltaTime;
+            Text_EMG.text = EMGStart.ToString("F2");
+        }
+    }
+
+    public void EMG_StartPauseBtn()
+    {
+        EMG_Active = !EMG_Active;
+        EMG_startPauseText.text = EMG_Active ? "PAUSE" : "START";
+    }
+
+    public void EMG_ResetBtn()
+    {
+        if(EMGStart > 0)
+        {
+            EMGStart = 0f;
+            Text_EMG.text = EMGStart.ToString("F2");
         }
     }
 }
