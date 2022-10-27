34de2be1143b1f234aeaaf16fb5e23146344d872
SNG0407 <kps1225@naver.com>
2022-10-27 10:34:44 +0900

    MouseInteraction_Test
    MouseInteraction_Test

---
+++ Haptic_Project/Assets/Scripts/MouseDraggable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDraggable : MonoBehaviour
+{
+    Vector3 mousePositionOffset;
+    private Vector3 GetMouseWorldPosition(){
+        //Capture mouse position & return worldPoint
+
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mouseScreenPos);
+    }
+    
+    private  void OnMouseDown() {
+        //Capture the mouse offset
+        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+        print("mousePositionOffset: "+mousePositionOffset+" = "+gameObject.transform.position+" - "+GetMouseWorldPosition());
+    }
+
+    private void OnMouseDrag() {
+        gameObject.transform.position = GetMouseWorldPosition() + mousePositionOffset;
+        print("Dragging...");
+        print("Position : "+gameObject.transform.position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
