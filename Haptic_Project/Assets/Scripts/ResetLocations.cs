302d9b50aeb9c4228d567d7a770a541c7835ea4a
SNG0407 <kps1225@naver.com>
2022-10-29 18:26:02 +0900

    UI prefab_reset location
    UI prefab_reset location
    오브젝트 위치 초기화 버튼 생성

---
+++ Haptic_Project/Assets/Scripts/ResetLocations.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLocations : MonoBehaviour
+{
+    private MouseDraggable[] ResetObjects; //리셋할 오브젝트 배열
+    private Vector3[] InitialLocations;   // 오브젝트들의 처음 위치 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //FindObjectOfType<UImanager>();
+        ResetObjects = FindObjectsOfType<MouseDraggable>(); //MouseDraggable 스크립트가 있는 오브젝트만 적용
+        InitialLocations = new Vector3 [(ResetObjects.Length)]; //
+        for(int i =0; i< ResetObjects.Length; i++){
+            print(ResetObjects[i].name);
+            InitialLocations[i]= new Vector3(ResetObjects[i].transform.position.x, ResetObjects[i].transform.position.y, ResetObjects[i].transform.position.z);
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetLocation(){
+        for(int i =0; i< ResetObjects.Length; i++){
+                    print(ResetObjects[i].name+" : "+InitialLocations[i]);
+                    //print(ResetObjects[i].name+" : "+ResetObjects[i].transform.position);
+                    ResetObjects[i].transform.position = new Vector3(InitialLocations[i].x,InitialLocations[i].y,InitialLocations[i].z);
+        }
+    }
+}
