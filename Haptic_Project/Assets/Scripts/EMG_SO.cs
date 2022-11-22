3b2cb867d36bded87cd69694c94adfa317cc0a1f
SNG <ksc0407@khu.ac.kr>
2022-11-22 22:50:14 +0900

    유선

--- Haptic_Project/Assets/Scripts/EMG_SO.cs
+++ Haptic_Project/Assets/Scripts/EMG_SO.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing.Imaging;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
--- Haptic_Project/Assets/Scripts/SerialCommunicator.cs
+++ Haptic_Project/Assets/Scripts/SerialCommunicator.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using ArduinoBluetoothAPI;
+using System.IO.Ports;
 
 
 public class SerialCommunicator : MonoBehaviour
 {
+    public SerialPort serial = new SerialPort("COM11", 9600); //here change port - where you have connected arduino to computer
+
+
     public TMP_InputField inputText;
     public TextMeshProUGUI receivedText;
     private BluetoothHelper helper;
@@ -44,48 +48,65 @@ public class SerialCommunicator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        deviceName = "HC-06";
-        try
-        {
-            helper = BluetoothHelper.GetInstance(deviceName);
-            helper.OnConnected += OnConnected;
-            helper.OnConnectionFailed += OnConnFailed;
 
-            helper.setTerminatorBasedStream("\n");
+        // deviceName = "HC-06";
+        // try
+        // {
+        //     helper = BluetoothHelper.GetInstance(deviceName);
+        //     helper.OnConnected += OnConnected;
+        //     helper.OnConnectionFailed += OnConnFailed;
 
-            if (helper.isDevicePaired())
-            {
-                helper.Connect();
-                Debug.Log("BT is Paired");
-            }
-        }
-        catch (BluetoothHelper.BlueToothNotEnabledException ex)
-        {
-            Debug.LogError($"BlueToothNotEnabledException : {ex.Message}");
-            this.enabled = false;
-        }
-        catch (Exception e)
-        {
-            Debug.LogError($"bt error : {e.Message}");
-        }
-        finally
-        {
-            // gameObject.SetActive(false);
-        }
+        //     helper.setTerminatorBasedStream("\n");
 
-        if (AppManager.Instance.IsTest)
-        {
-            gameObject.SetActive(false);
-            return;
-        }
+        //     if (helper.isDevicePaired())
+        //     {
+        //         helper.Connect();
+        //         Debug.Log("BT is Paired");
+        //     }
+        // }
+        // catch (BluetoothHelper.BlueToothNotEnabledException ex)
+        // {
+        //     Debug.LogError($"BlueToothNotEnabledException : {ex.Message}");
+        //     this.enabled = false;
+        // }
+        // catch (Exception e)
+        // {
+        //     Debug.LogError($"bt error : {e.Message}");
+        // }
+        // finally
+        // {
+        //     // gameObject.SetActive(false);
+        // }
+
+        // if (AppManager.Instance.IsTest)
+        // {
+        //     gameObject.SetActive(false);
+        //     return;
+        // }
+
 
         StartCoroutine(Send());
     }
+  
+    // IEnumerator Send()
+    // {
+    //     yield return new WaitUntil(() => helper.isConnected());
 
+    //     while (true)
+    //     {
+    //         int pressure1 = (int)(handControllerSo.pressureRight[0].fingerPressure * 100);
+    //         int pressure2 = (int)(handControllerSo.pressureRight[1].fingerPressure * 100);
+    //         int pressure3 = (int)(handControllerSo.pressureRight[2].fingerPressure * 100);
+    //         string msg = $"<{pressure1},{pressure2},{pressure3}>";
+    //         BTsend(msg);
+    //         yield return new WaitForSeconds(0.5f);
+    //     }
+    // }
 
     IEnumerator Send()
     {
-        yield return new WaitUntil(() => helper.isConnected());
+
+                 serial.Open();
 
         while (true)
         {
@@ -93,26 +114,34 @@ public class SerialCommunicator : MonoBehaviour
             int pressure2 = (int)(handControllerSo.pressureRight[1].fingerPressure * 100);
             int pressure3 = (int)(handControllerSo.pressureRight[2].fingerPressure * 100);
             string msg = $"<{pressure1},{pressure2},{pressure3}>";
-            Debug.Log($"sendMsg : {msg}");
-            BTsend(msg);
-            // yield return new WaitForSeconds(sampleTime);
+            if (serial.IsOpen)
+            {
+                serial.Write(msg);
+                Debug.Log(msg);
+
+            }
+
             yield return new WaitForSeconds(0.5f);
+
         }
+        serial.Close();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (helper.Available)
-        {
-            string msg = helper.Read();
-            string[] emgDatas = msg.Split(',');
-            int grabEmg = Convert.ToInt32(emgDatas[0]);
-            int pickEmg = Convert.ToInt32(emgDatas[1]);
-            // Debug.Log($"recv grabEmg : {grabEmg} / pickEmg : {pickEmg}");
-            emgSO.PushData(EMG_SO.EMGType.GRAB, grabEmg);
-            emgSO.PushData(EMG_SO.EMGType.PICK, pickEmg);
-        }
+        // if (helper.Available)
+        // {
+        //     string msg = helper.Read();
+        //     string[] emgDatas = msg.Split(',');
+        //     int grabEmg = Convert.ToInt32(emgDatas[0]);
+        //     int pickEmg = Convert.ToInt32(emgDatas[1]);
+        //     // Debug.Log($"recv grabEmg : {grabEmg} / pickEmg : {pickEmg}");
+        //     emgSO.PushData(EMG_SO.EMGType.GRAB, grabEmg);
+        //     emgSO.PushData(EMG_SO.EMGType.PICK, pickEmg);
+        // }
     }
 
     public void SendData_Btn(string data)
@@ -123,14 +152,24 @@ public class SerialCommunicator : MonoBehaviour
 
     private void BTsend(string data)
     {
-        if (helper.IsBluetoothEnabled())
+        try
         {
-            helper.SendData(data);
+            // if (helper.IsBluetoothEnabled())
+            {
+                helper.SendData(data);
+                Debug.Log($"sendMsg : {data}");
+
+            }
+            //     else
+            //     {
+            //         Debug.LogWarning($"bluetooth is not enabled");
+            //     }
         }
-        else
+        catch (Exception e)
         {
-            Debug.LogWarning($"bluetooth is not enabled");
+            Debug.LogWarning($"BT send exception : ${e}");
         }
+
     }
 
     private void UpdateReceivedData()
