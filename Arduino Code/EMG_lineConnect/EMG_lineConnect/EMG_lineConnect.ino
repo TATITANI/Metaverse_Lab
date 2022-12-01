f944b193b826274726b037bfefef142a6da39d61
SNG0407 <ksc0407@khu.ac.kr>
2022-12-01 18:01:20 +0900

    근전도 아두이노 코드

---
+++ Arduino Code/EMG_lineConnect/EMG_lineConnect/EMG_lineConnect.ino
@@ -0,0 +1,131 @@
+///////EMG
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include "EMGFilters.h"
+#define TIMING_DEBUG 1
+#define SensorInputPin A0    // input pin number
+#define SensorInputPin_1 A1  // input pin number
+
+EMGFilters myFilter;
+// discrete filters must works with fixed sample frequence
+// our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
+// other sampleRate inputs will bypass all the EMG_FILTER
+int sampleRate = 500;//SAMPLE_FREQ_500HZ;
+// For countries where power transmission is at 50 Hz
+// For countries where power transmission is at 60 Hz, need to change to
+// "NOTCH_FREQ_60HZ"
+// our emg filter only support 50Hz and 60Hz input
+// other inputs will bypass all the EMG_FILTER
+int humFreq = NOTCH_FREQ_50HZ;
+
+// Calibration:
+// put on the sensors, and release your muscles;
+// wait a few seconds, and select the max value as the threshold;
+// any value under threshold will be set to zero
+static int Threshold_Min = 0;      //유효 최소값
+static int Threshold_Max = 700;    //200000; //유효 최대값
+static int Threshold_Min_1 = 0;    //유효 최소값
+static int Threshold_Max_1 = 700;  //200000; //유효 최대값 //공 쥐는 거 : 700 / 집는거 손등 85
+
+
+unsigned long timeStamp;
+unsigned long timeBudget;
+unsigned long timeMillis;
+unsigned long startMillis;
+unsigned long currentMillis;
+unsigned long previousMillis;
+const long interval = 10000;  //120,000=2분(120,000ms)
+int count = 1;
+unsigned long sum = 0;  // 합
+unsigned long ave = 0;  // 평균
+unsigned long maxEMG = 0;
+
+char result[8];
+char* ch1;
+
+String sendData, recvData;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  myFilter.init(sampleRate, humFreq, true, true, true);
+  // setup for time cost measure
+  // using micros()
+  timeBudget = 1e6 / sampleRate;
+  timeMillis = 0;
+  startMillis = millis();
+}
+
+
+// EMG 블루투스 전송
+void SendData() {
+  // In order to make sure the ADC sample frequence on arduino,
+  // the time cost should be measured each loop
+  /*------------start here-------------------*/
+  timeStamp = micros();
+
+  int Value = analogRead(SensorInputPin);
+  //SensorInputPin_1
+  int Value_1 = analogRead(SensorInputPin_1);
+
+  // filter processing
+  int DataAfterFilter = myFilter.update(Value);
+  int DataAfterFilter_1 = myFilter.update(Value_1);
+
+  int envlope = sq(DataAfterFilter);
+  int envlope_1 = sq(DataAfterFilter_1);
+
+  // any value under threshold will be set to zero
+  envlope = (envlope > Threshold_Min) ? envlope : 0;
+  envlope_1 = (envlope_1 > Threshold_Min_1) ? envlope_1 : 0;
+
+  timeStamp = micros() - timeStamp;
+  if (TIMING_DEBUG) {
+
+    //if(envlope !=0 && envlope<Threshold_Max){
+    if (envlope < Threshold_Max || envlope_1 < Threshold_Max_1) {
+      //Serial.println("DATA,TIME,TIMER," + String(millis()*0.001));
+
+      String str_envlope = String(envlope);
+      String str_envlope_1 = String(envlope_1);
+
+      //sendData = "0, "+str_envlope + ',' + str_envlope_1+", 4000";  //테스트용
+      sendData = "#, "+str_envlope + ',' + 0;    //유니티용
+
+      char* a = new char[sendData.length() + 1];
+      strcpy(a, sendData.c_str());
+      //delay(200);  //excels
+      if (maxEMG < envlope) {
+        maxEMG = envlope;  //최대값 maxEMG
+      }
+      count++;
+      sum = sum + envlope;
+      //Serial.write(a);
+      Serial.println(a);
+      //mySerial.println(a);  // 전송
+      delete a;
+    }
+
+    // Serial.println("Filters cost time: " + String(timeStamp));
+    // the filter cost average around 520 us
+  }
+}
+
+
+
+
+void loop() {
+  SendData();
+
+  // put your main code here, to run repeatedly:
+  //delayMicroseconds(50000);
+  delay(50);
+}
+//서보 : 가변저항 : <180,180,180> : 600 450 477
+//서보 : 가변저항 : <0,0,0> :  1013  1000 1023
+//<0,0,0>       873 1023
+//<90,90,90>
